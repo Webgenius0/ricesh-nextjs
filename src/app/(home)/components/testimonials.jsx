3b3e9ac16e9423f4736607ef36e4b5904107c62f
Asaduzzaman70 <asdudjamanhisam@gmail.com>
2025-05-04 14:15:33 +0600

    add embla-carousel-react and implement Carousel component for enhanced testimonials display

--- src/app/(home)/components/testimonials.jsx
+++ src/app/(home)/components/testimonials.jsx
@@ -1,3 +1,12 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
 export default function Testimonials() {
   return (
     <section>
@@ -16,14 +25,14 @@ export default function Testimonials() {
             opts={{
               align: "start",
             }}
-            className="w-full max-w-sm"
+            className="w-full"
           >
             <CarouselContent>
               {Array.from({ length: 5 }).map((_, index) => (
-                <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                <CarouselItem key={index} className={`md:basis-1/2 lg:basis-1/3 w-full ${index === 1 ? "scale-150" : ""}`}>
                   <div className="p-1">
-                    <Card>
-                      <CardContent className="flex aspect-square items-center justify-center p-6 sca">
+                    <Card className="">
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
                         <span className="text-3xl font-semibold">
                           {index + 1}
                         </span>
