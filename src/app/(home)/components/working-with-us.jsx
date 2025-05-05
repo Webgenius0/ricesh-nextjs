d6c0969abf51e77c3961567c2d69d8a2ba8c479d
Asaduzzaman70 <asdudjamanhisam@gmail.com>
2025-05-05 11:15:01 +0600

    feat: implement step component in WorkingWithUs section and update layout in Home page

--- src/app/(home)/components/working-with-us.jsx
+++ src/app/(home)/components/working-with-us.jsx
@@ -8,9 +8,42 @@ export default function WorkingWithUs() {
           </h1>
         </div>
         <div>
-          
+          <div className="flex flex-col items-center mt-10 space-y-0 mb-40">
+            <Step isCompleted={false} />
+            <Step isCompleted={true} />
+            <Step isCompleted={false} />
+            <Step isCompleted={true} />
+            <Step isCompleted={false} />
+            <Step isCompleted={true} />
+            <Step isCompleted={false} step={"last"} />
+          </div>
         </div>
       </div>
     </section>
+  );
+}
+
+function Step({ isCompleted, step }) {
+  return (
+    <div className={` ${isCompleted ? "flex flex-row-reverse" : "flex"}`}>
+      <div className="relative flex flex-col items-center">
+        <div className="w-6 h-6 rounded-full bg-cyan-500 flex items-center justify-center z-10">
+          {isCompleted ? (
+            <span className="text-white">&#10003;</span>
+          ) : (
+            <span />
+          )}
+        </div>
+        <div
+          className={`${step === "last" ? "h-0" : "w-1 h-36 bg-cyan-500"}`}
+        ></div>
+      </div>
+      <div className={`absolute max-w-[530px] bg-white shadow-accent p-6 border-2 ${isCompleted ? "mr-10" : "ml-10"}`}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quas
+        deleniti quo, itaque molestiae voluptatibus ad quisquam quis atque
+        voluptate nobis quidem doloremque vitae dignissimos, placeat ullam est
+        mollitia ipsa, cumque ex iure! Animi, commodi? Quae rerum libero ad
+      </div>
+    </div>
   );
 }
--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -22,7 +22,7 @@ export default function Home() {
       <WhyChooseUs />
       <WhatDrivesUs />
       <WeWontSettle />
-      {/* <WorkingWithUs /> */}
+      <WorkingWithUs />
       <Testimonials />
       <GetInTouch />
       {/* <Components /> */}
