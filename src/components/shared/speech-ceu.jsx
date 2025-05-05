162f08500386cf02384e987e71e116715fccb697
asaduzzaman70 <asdudjamanhisam@gmail.com>
2025-05-05 17:35:59 +0600

    feat: add Progress component and integrate into SpeechCeu component

--- src/components/shared/speech-ceu.jsx
+++ src/components/shared/speech-ceu.jsx
@@ -8,39 +8,115 @@ import {
   useDotButton,
 } from "../ui/carousel";
 import { cn } from "@/lib/utils";
+import { Progress } from "../ui/progress";
+import { Button } from "../ui/button";
 
 export default function SpeechCeu() {
   return (
     <section>
       <div className="container py-[100px]">
-        <Carousel opts={{ align: "center" }} className="w-full relative">
+        <Carousel opts={{ align: "center" }} className="w-full relative rounded-2xl overflow-hidden">
           <CarouselContent className="items-center">
             <CarouselItem className={cn("w-full md:basis-1/2 lg:basis-full")}>
-              <div className="grid grid-cols-2 h-full">
-                <div className="">
-                  <h3>SpeechCEU | Empowers | SLPs</h3>
-                  <h1 className="">
+              <div className="flex gap-20 h-ful">
+                <div className="flex-1 pt-10">
+                  <h3 className="text-base">SpeechCEU | Empowers | SLPs</h3>
+                  <h1 className="text-4xl font-semibold leading-[44px] text-foreground mt-9 mb-12">
                     SpeechCEU Empowers SLPs to Thrive with Expert-Led Continuing
                     Education
                   </h1>
+                  <div className="grid grid-cols-2 gap-10 mb-12">
+                    <div>
+                      <div className="flex items-end mb-3">
+                        <p className="text-sm font-normal">
+                          Lorem Ipsum is simply. dummy text of the printing...
+                        </p>
+                        <h4 className="text-2xl font-medium text-primary">
+                          97%
+                        </h4>
+                      </div>
+                      <div>
+                        <Progress className="h-3" value={97} />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="flex items-end mb-3">
+                        <p className="text-sm font-normal">
+                          Lorem Ipsum is simply. dummy text of the printing...
+                        </p>
+                        <h4 className="text-2xl font-medium text-primary">
+                          56%
+                        </h4>
+                      </div>
+                      <div>
+                        <Progress className="h-3" value={56} />
+                      </div>
+                    </div>
+                  </div>
                   <div>
-                    <div></div>
+                    <Button className="w-fit" variant={"default"} size="sm">
+                      Read Full Story
+                    </Button>
                   </div>
                 </div>
-                <div className="rounded-3xl overflow-hidden">
+                <div className="rounded-3xl overflow-hidden w-1/2 max-h-[520px]">
+                  <img
+                    src="/images/speech-ceu.jpg"
+                    alt=""
+                    className="object-contain"
+                  />
+                </div>
+              </div>
+            </CarouselItem>
+            <CarouselItem className={cn("w-full md:basis-1/2 lg:basis-full")}>
+              <div className="flex gap-20 h-ful">
+                <div className="flex-1 pt-16">
+                  <h3 className="text-base">SpeechCEU | Empowers | SLPs</h3>
+                  <h1 className="text-4xl font-semibold leading-[44px] text-foreground mt-9 mb-12">
+                    SpeechCEU Empowers SLPs to Thrive with Expert-Led Continuing
+                    Education
+                  </h1>
+                  <div className="grid grid-cols-2 gap-10 mb-12">
+                    <div>
+                      <div className="flex items-end mb-3">
+                        <p className="text-sm font-normal">
+                          Lorem Ipsum is simply. dummy text of the printing...
+                        </p>
+                        <h4 className="text-2xl font-medium text-primary">
+                          97%
+                        </h4>
+                      </div>
+                      <div>
+                        <Progress className="h-3" value={97} />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="flex items-end mb-3">
+                        <p className="text-sm font-normal">
+                          Lorem Ipsum is simply. dummy text of the printing...
+                        </p>
+                        <h4 className="text-2xl font-medium text-primary">
+                          56%
+                        </h4>
+                      </div>
+                      <div>
+                        <Progress className="h-3" value={56} />
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <Button className="w-fit" variant={"default"} size="sm">
+                      Read Full Story
+                    </Button>
+                  </div>
+                </div>
+                <div className="rounded-3xl overflow-hidden w-1/2">
                   <img src="/images/speech-ceu.jpg" alt="" />
                 </div>
               </div>
             </CarouselItem>
-            <CarouselItem
-              className={cn(
-                "w-full md:basis-1/2 lg:basis-full bg-amber-200 h-28"
-              )}
-            >
-              <div>Hisam</div>
-            </CarouselItem>
           </CarouselContent>
-          <CarouselDots className="" />
+          <CarouselDots className="absolute bottom-7 left-2" />
         </Carousel>
       </div>
     </section>
