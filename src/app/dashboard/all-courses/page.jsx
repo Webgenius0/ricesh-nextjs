5fb729cbd3b2d87a6a18d531a490147fb5d884e3
asaduzzaman70 <asdudjamanhisam@gmail.com>
2025-05-08 17:39:25 +0600

    feat: add CurrentStreak component and integrate video and image assets

--- src/app/dashboard/all-courses/page.jsx
+++ src/app/dashboard/all-courses/page.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import BasedOnYourCareerGoals from "./components/based-on-your-career-goals";
 import CourseProgress from "./components/course-progress";
 import CareerGoalsSelector from "./components/career-goals-selector";
+import CurrentStreak from "./components/current-streak";
 
 export default function page() {
   return (
     <div>
+      <CurrentStreak />
       <CareerGoalsSelector />
       <CourseProgress />
       <BasedOnYourCareerGoals />
