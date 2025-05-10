3a2555a09b67978ffe93fca7186177c38734386f
asaduzzaman70 <asdudjamanhisam@gmail.com>
2025-05-10 11:05:07 +0600

    feat: add Overview component with card display for team metrics and associated SVG icons

--- src/app/admin-dashboard/page.jsx
+++ src/app/admin-dashboard/page.jsx
@@ -1,9 +1,11 @@
+import Overview from "./components/overview";
 import TopButtonSection from "./components/top-button-section";
 
 export default function page() {
   return (
     <div>
       <TopButtonSection />
+      <Overview />
     </div>
   );
 }
