49a42cfb6fb89fe4f6a2defd37bed17df01d0b8c
asaduzzaman70 <asdudjamanhisam@gmail.com>
2025-05-14 13:02:15 +0600

    refactor: enhance layout and responsiveness across multiple components

--- src/app/continuing-education/components/learning-focused.jsx
+++ src/app/continuing-education/components/learning-focused.jsx
@@ -35,19 +35,19 @@ const sideCardData = [
 export default function LearningFocus() {
   return (
     <section data-aos="fade-up" className="bg-dark-foreground/5">
-      <div className="container pt-[120px] pb-[126px]">
+      <div className="container py-[24px] md:pt-[120px] md:pb-[126px] px-5 md:px-8">
         <div>
-          <h2 className="text-3xl font-semibold font-outfit text-foreground">
+          <h2 className="text-lg lg:text-3xl font-semibold font-outfit text-foreground">
             Learning focused on your goals
           </h2>
         </div>
-        <div className="grid grid-cols-2 mt-9 gap-6">
-          <div className="space-y-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 mt-9 gap-6">
+          <div className="space-y-5 md:space-y-6">
             {sideCardData?.map((data, idx) => (
               <SideCard key={idx} data={data} />
             ))}
           </div>
-          <div className="bg-no-repeat bg-center bg-cover bg-[url(/images/weWontSettleSideImage.png)] rounded-3xl"></div>
+          <div className="bg-no-repeat bg-center bg-cover bg-[url(/images/weWontSettleSideImage.png)] rounded-3xl min-h-[496px] md:h-auto"></div>
         </div>
       </div>
     </section>
@@ -61,24 +61,24 @@ function SideCard({ data }) {
         !data?.linkPath && "border-primary border-b-8"
       }`}
     >
-      <div className="flex items-start gap-5 overflow-x-hidden">
+      <div className="flex items-start gap-3 lg:gap-5 overflow-x-hidden">
         <div>
-          <div className="w-[72px] h-[72px]">
+          <div className="w-[60px] lg:w-[72px] h-[60px] lg:h-[72px]">
             <Clipboard />
           </div>
         </div>
         <div>
-          <h5 className="text-2xl font-semibold font-outfit line-clamp-2">
+          <h5 className="text-base lg:text-2xl font-semibold font-outfit line-clamp-2">
             {data?.title}
           </h5>
-          <p className="line-clamp-1 text-base mt-2 mb-4">
+          <p className="line-clamp-1 text-xs lg:text-base mt-2 mb-4">
             {data?.description}
           </p>
           {data?.linkPath && (
             <div>
               <Link
                 href={data?.linkPath}
-                className="text-base text-primary underline flex items-center gap-1 w-fit group"
+                className="text-sm md:text-base text-primary underline flex items-center gap-1 w-fit group"
               >
                 Explore Courses{" "}
                 <div className="group-hover:translate-x-2 duration-200 ease-in-out">
