92a2fac676d31ec98d2166e3cf8819766773c1bd
asaduzzaman70 <asdudjamanhisam@gmail.com>
2025-05-18 16:12:41 +0600

    Add ContinueEducationPartner component to continuing education page

--- src/app/continuing-education/page.jsx
+++ src/app/continuing-education/page.jsx
@@ -5,6 +5,7 @@ import LearningFocus from "./components/learning-focused";
 import LevelUpSection from "./components/level-up-section";
 import DiscoverHowLearning from "./components/discover-how-learning";
 import ContinueEducationLearningModule from "./components/continue-education-learning-module";
+import ContinueEducationPartner from "./components/continue-education-partners";
 
 export default function page() {
   return (
@@ -15,6 +16,7 @@ export default function page() {
       <LearningFocus />
       <LevelUpSection />
       <DiscoverHowLearning />
+      <ContinueEducationPartner />
       <SpeechCeu />
     </div>
   );
