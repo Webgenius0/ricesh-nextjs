2c4d0e0ecd9a88951ffd621eadd444ecc690c32b
asaduzzaman70 <asdudjamanhisam@gmail.com>
2025-05-19 10:22:22 +0600

    refactor: enhance layout and responsiveness in admin control panel components

--- src/app/admin-control-panel/components/admin-control-panel-header.jsx
+++ src/app/admin-control-panel/components/admin-control-panel-header.jsx
@@ -1,11 +1,11 @@
 export default function AdminControlPanelHeader() {
   return (
     <section className="bg-dark">
-      <div className="container py-6 mb-8">
-        <h2 className="text-[32px] font-semibold font-outfit text-white">
+      <div className="container py-6 mb-8 px-5 md:px-8">
+        <h2 className="text-xl md:text-2xl lg:text-[32px] font-semibold font-outfit text-white">
           Admin Control Panel
         </h2>
-        <p className="text-base font-medium mt-2.5 text-dark-foreground">
+        <p className="text-xs md:text-sm lg:text-base md:font-medium mt-2.5 text-dark-foreground">
           Manage user certifications, track progress, and oversee compliance.
         </p>
       </div>
